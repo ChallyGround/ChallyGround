648057d6efa8adbda847ac928faa852e2b470690
RACENI <saracen_dev@naver.com>
2024-07-16 23:50:06 +0900

    etc: TEST

--- ChallyGround/src/main/java/com/chally/example/controller/testController.java
+++ ChallyGround/src/main/java/com/chally/example/controller/testController.java
@@ -1,10 +1,12 @@
-package com.chally.example;
+package com.chally.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chally.example.service.TestService;
 
 @Controller
 public class testController {
