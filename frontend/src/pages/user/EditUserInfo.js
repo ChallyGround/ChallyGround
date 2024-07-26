7e20521e2d082918a8a6c811753f205133cf407d
RACENI <saracen_dev@naver.com>
2024-07-27 01:07:00 +0900

    publishing: SWAL추가

--- frontend/src/pages/user/EditUserInfo.js
+++ frontend/src/pages/user/EditUserInfo.js
@@ -1,5 +1,6 @@
 // EditUserInfo.js
 import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 
 function EditUserInfo({ closeEditUserInfo }) {
     const [name, setName] = useState('');
@@ -11,6 +12,18 @@ function EditUserInfo({ closeEditUserInfo }) {
         alert(`Name: ${name}, Email: ${email}, Telephone: ${telephone}`);
         closeEditUserInfo();
     };
+
+    const handleSaveUserInfo = () => {
+        // 예제 저장 로직
+        console.log('저장:', { name, email, telephone });
+    
+        Swal.fire({
+          title: '저장 완료!',
+          text: '회원정보가 성공적으로 저장되었습니다.',
+          icon: 'success',
+          confirmButtonText: '확인'
+        });
+      };
 
     return (
         <div className="container">
@@ -29,7 +42,7 @@ function EditUserInfo({ closeEditUserInfo }) {
                     <input type="tel" id="telephone" name="telephone" value={telephone} onChange={(e) => setTelephone(e.target.value)} required />
                 </div>
                 <div className="form-group">
-                    <button type="button" onClick={saveUserInfo}>저장</button>
+                    <button type="button" onClick={handleSaveUserInfo}>저장</button>
                 </div>
             </form>
         </div>
